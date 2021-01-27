968ffe6a8598da9928e097afa322f255c4747d6b
Arun Saini <gog1withme@gmail.com>
2021-01-27 12:54:01 +0530

    changed getitems to get

--- amplify/backend/function/apilambda/src/app.js
+++ amplify/backend/function/apilambda/src/app.js
@@ -120,17 +120,14 @@ app.get(path, function (req, res) {
   }
 
   var params = {
-    TableName: tableClients,
-    Select: "ALL_ATTRIBUTES",
+    TableName: tableClients,   
     Key : { 
-      "email" : {
-        "S" : req.query.email
-      }
+      HashKey: "email"
     }
   };
 
   console.log("DDB param: ", params);
-  dynamodb.getItem(params, function (err, data) {
+  dynamodb.get(params, function (err, data) {
     if (err) {      
       console.error("Unable to read item. Error JSON:", JSON.stringify(err));
     } else {
