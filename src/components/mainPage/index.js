0c3863fa815e18a29c20cf64c487faee2b8e06fe
Arun Saini <gog1withme@gmail.com>
2021-01-30 01:13:49 +0530

    Email notification code

--- src/components/mainPage/index.js
+++ src/components/mainPage/index.js
@@ -132,9 +132,9 @@ export default function Index() {
     const formSend = async () => {
       if (!email) return;
 
-      // await fetch(`https://i6smufsvj6.execute-api.us-east-1.amazonaws.com/live/visit?email=${email}`, {
-      //   mode: 'no-cors',
-      // });
+      await fetch(`https://i6smufsvj6.execute-api.us-east-1.amazonaws.com/live/visit?email=${email}`, {
+        mode: 'no-cors',
+      });
     };
     formSend();
   }, []);
