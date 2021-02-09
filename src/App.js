bd00410083faa50e2835b1b45f212bcf0c44d9bd
Arun Saini <gog1withme@gmail.com>
2021-02-10 01:49:33 +0530

    Import resizeobserver dynamically

--- src/App.js
+++ src/App.js
@@ -9,6 +9,21 @@ import Clients from './components/clients'
 import Control from './components/admin/control'
 
 export default function App() {
+
+  (async () => {
+    try{
+    if ('ResizeObserver' in window === false) {
+      // Loads polyfill asynchronously, only if required.
+      const module = await import('@juggle/resize-observer');
+      window.ResizeObserver = module.ResizeObserver;
+    }
+    // Uses native or polyfill, depending on browser support.
+    const ro = new ResizeObserver((entries, observer) => {
+      console.log('Something has resized!');
+    });
+  }catch{}
+  })();
+
   return (
     <>
       <Switch>
--- src/index.js
+++ src/index.js
@@ -5,15 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ResizeObserver from 'resize-observer-polyfill';
- 
-const ro = new ResizeObserver((entries, observer) => {
-    for (const entry of entries) {
-        const {left, top, width, height} = entry.contentRect; 
-    }
-});
- 
-ro.observe(document.body);
+
 
 ReactDOM.render(
   <Router>
